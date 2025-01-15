eab42534e21d1ebc568ab018c03fd3d87025d1f8
nuril izza <141518206+brwnsweet@users.noreply.github.com>
2025-01-15 12:24:34 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -6,164 +6,49 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" 
-    integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" 
-    crossorigin="anonymous">
-    <link rel="stylesheet" href="assets/css/style.css">
-    <title>Buku Tamu</title>
-    <style>
-        html{
-            scroll-behavior: smooth;
-        }
-    </style>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
+    <title>Admin Tamu</title>
   </head>
   <body>
-    <!-- Navbar -->
-     <section id="nav">
-     <nav class="navbar navbar-expand-lg navbar-light">
-    
-  <a class="navbar-brand" href="#"><span>Buku Tamu</span></a>
+  <nav class="navbar navbar-expand-lg navbar-light bg-info fixed-top">
+  <a class="navbar-brand text-white" href="#"><i class="fas fa-tv mr-1"></i>
+SELAMAT DATANG ADMIN | <b>BUKU TAMU</b></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav ml-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#progkal">Program Keahlian</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#visimisi">Visi Misi</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#contact">Hubungi Kami</a>
-      </li>
-    </ul>
-  </div>
- 
 </nav>
-     </section>
-     <!-- banner -->
-     <section id="banner">
-        <div class="banner">
-                <div class="row">
-                    <div class="col-md-6">
-                        <h4 class="title">Selamat Datang di SMKN 9 Malang</h4>
-                        <p>Terima Kasih telah berkunjung ke Buku Tamu ini. Semoga Anda Sehat Selalu</p>
-                    </div>
-                    <div class="col-md-6">
-                        <img src="assets/images/head.png" class="ban-img img-fluid"></img>
-                    </div>
-                </div>
-        </div>
-        <svg  class="wave" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#fff" fill-opacity="1" d="M0,224L48,218.7C96,213,192,203,288,197.3C384,192,480,192,576,202.7C672,213,768,235,864,234.7C960,235,1056,213,1152,186.7C1248,160,1344,128,1392,112L1440,96L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
-     </section>
-     <!-- bagian program keahlian -->
-      <section id="progkal">
-        <div class="container">
-            <h2 class="text-center">PROGRAM KEAHLIAN</h2>
-            <div class="row text-center">
-                <div class="col-md-3 progkal">
-                    <img src="assets/images/tbsm.png" class="img-fluid">
-                    <h4>Teknik Bisnis Sepeda Motor</h4>
-                    <p>Menghasilkan tenaga bersertifikasi dan kompeten
-                        untuk mengisi lowongan kerja di teknik otomotif Sepeda Motor, Jasa Perawatan Sepeda Motor 
-                        dan mampu bersaing di Tingkat Nasional maupun Internasional.
-                    </p>
-                </div>
-                <div class="col-md-3 progkal">
-                    <img src="assets/images/tkj.png" class="img-fluid">
-                    <h4>Teknik Komputer dan Jaringan</h4>
-                    <p>Menghasilkan tenaga bersertifikasi dan kompeten
-                        untuk mengisi lowongan kerja di bidang Informatika dan Komputer 
-                        dan mampu bersaing di Tingkat Nasional maupun Internasional.
-                    </p>
-                </div>
-                <div class="col-md-3 progkal">
-                    <img src="assets/images/rpl.png" class="img-fluid">
-                    <h4>Teknik Rekayasa Perangkat Lunak</h4>
-                    <p>Menghasilkan tenaga bersertifikasi dan kompeten
-                        untuk mengisi lowongan kerja di bidang Rekayasa Perangkat Lunak
-                        dan mampu bersaing di Tingkat Nasional maupun Internasional.
-                    </p>
-                </div>
-                <div class="col-md-3 progkal">
-                    <img src="assets/images/anm.png" class="img-fluid">
-                    <h4>Animasi</h4>
-                    <p>Menghasilkan tenaga bersertifikasi dan kompeten
-                        untuk mengisi lowongan kerja di bidang Animasi
-                        dan mampu bersaing di Tingkat Regional maupun Internasional.
-                    </p>
-                </div>
-            </div>
-        </div>
-      </section>
-      <!-- visi misi -->
-       <section id="visimisi">
-        <div class="container">
-            <h2 class="text-center">Visi dan Misi</h2>
-            <div class="row">
-                <div class="col-md-6">
-                    <h4>Visi Sekolah</h4>
-                    <p>MEWUJUDKAN LEMBAGA PENDIDIKAN DAN PELATIHAN KEJURUHAN, YANG MENGHASILKAN LULUSAN YANG BERAKHLAK MULIA,
-                        KOMPETEN, DAN BERWAWASAN LINGKUNGAN, SERTA MAMPU BERSAING DI TINGKAT NASIONAL MAUPUN INTERNASIONAL.
-                    </p><br>
-                    <h4>Misi Sekolah</h4>
-                    <ol>
-                        <li>Mewujudkan tenaga pendidik, kependidikan, dan lulusan yang berbudi pekerti dan berakhlak mulia.</li>
-                        <li>Mengelola management sekolah dengan standar ISO 9001 : 2015</li>
-                        <li>Melaksanakan sistem pendidikan dan mengupayakan mutu layanan pendidikan 
-                            sesuai tuntutan masyarakat dg memanfaatkan teknologi informasi dan komunikasi</li>
-                        <li>Mewujudkan lingkungan sekolah menuju sekolah adiwiyata serta mengembangkan nilai-nilai kewirausahaan warga sekolah.</li>
-                    </ol>
-                </div>
-                <div class="col-md-6">
-                    <img src="assets/images/target.png" class="img-fluid">
-                </div>
-            </div>
-        </div>
-       </section>
-       <!--Bagian contact -->
-       <section id="contact">
-        <div class="container">
-            <h2 class="text-center">HUBUNGI KAMI</h2>
-            <div class="row contact">
-                <div class="col-md-4">
-                <img src="assets/images/contact-us.png" class="img-fluid">
-                </div>
-                <div class="col-md-8">
-                    <form action="proses.php" method="post">
-                        <div class="form-group">
-                            <label>No KTP</label>
-                            <input type="number" class="form-control" name="noktp" placeholder="No KTP anda...">
-                        </div>
-                        <div class="form-group">
-                            <label>No Handphone</label>
-                            <input type="number" class="form-control" name="nohp" placeholder="No Handphone anda...">
-                        </div>
-                        <div class="form-group">
-                            <label>Nama</label>
-                            <input type="text" class="form-control" name="nm" placeholder="Nama anda...">
-                        </div>
-                        <div class="form-group">
-                            <label>Alamat</label>
-                            <textarea class="form-control" rows="3" name="alm" placeholder="Alamat anda..."></textarea>
-                        </div>
-                        <button class="btn btn-primary w-100">KIRIM</button>
-                    </form>
-                </div>
-            </div>
-        </div>
-       </section>
-       <!-- footer -->
-        <section id="footer">
-            <div class="container">
-                <p class="text-center">Copyright &copy;2025</p>
-            </div>
-        </section>
-
+<div class="row no-gutters mt-5">
+  <div class="col-md-2 bg-dark mt-2 pr-3 pt-4">
+  <ul class="nav flex-column">
+  <li class="nav-item">
+    <a class="nav-link active text-white" href="index.php"><i class="fas fa-home mr-2"></i>Home</a>
+    <hr class="bg-warning">
+  </li>
+  <li class="nav-item">
+    <a class="nav-link text-white" href="index.php?halaman=datatamu"><i class="fas fa-tasks mr-2"></i>Data Tamu</a>
+    <hr class="bg-warning">
+  </li>
+  <li class="nav-item">
+    <a class="nav-link text-white" href="index.php?halaman=logout"><i class="fas fa-sign-out-alt mr-2"></i>Log Out</a>
+    <hr class="bg-warning">
+  </li>
+</ul>
+  </div>
+  <div class="col-md-10 p-5 pt-2">
+    <?php
+    if(isset($_GET['halaman'])){
+      if($_GET['halaman']== 'datatamu'){
+        include 'datatamu.php';
+    }elseif($_GET['halaman']== 'logout'){
+      include 'logout.php';
+    }
+  }else{
+    include 'home.php';
+  }
+    ?>
+  </div>
+</div>
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
